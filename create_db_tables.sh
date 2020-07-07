99ec3e4cdedbbc8bbac360e7099b742a81b64f3e
Miguel <Miguelrchaves@gmail.com>
2020-07-08 00:09:53 +0100

    Thesis parte1

--- create_db_tables.sh
+++ create_db_tables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-DB_CONTAINER_NAME="eramba-community-docker_db_1"
-#DB_SCHEMA_SCRIPT="./sql/c2.4.1.sql"
-DB_SCHEMA_SCRIPT="./sql/c2.8.1.sql"
-
-docker exec -i ${DB_CONTAINER_NAME} sh -c \
+DB_SCHEMA_SCRIPT="sql/c2.8.1.sql"
+docker-compose exec  app sh -c  'mkdir -p /var/www/sites/eramba_community/app/webroot/{files,backups}' #criaçao de pastas dentro de container para evitar volumes
+docker-compose exec  app sh -c  'mkdir -p /var/www/sites/eramba_community/app/tmp/logs' 
+CONTAINER=$(docker inspect -f '{{.Name}}' $(docker-compose ps -q db) | cut -c2-)
+#ver o nome do processo 
+docker exec -i ${CONTAINER} sh -c \
     'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE"' < ${DB_SCHEMA_SCRIPT}
--- prep_host_directory_struct.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-BASE_DIR="/data/eramba"
-mkdir -p ${BASE_DIR}/mysql ${BASE_DIR}/files ${BASE_DIR}/backups ${BASE_DIR}/erambalogs ${BASE_DIR}/systemlogs
